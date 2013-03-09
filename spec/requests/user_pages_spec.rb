fae49827d3246712c46d12322f3c11ebaa95d7a0
rambz <rambz1@gmail.com>
2013-03-09 20:16:46 +0200

    Finish layout and routes

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,10 +4,17 @@ describe "User Pages" do
   
   subject { page }
   
+  shared_examples_for "all user actions" do
+    it { should have_selector('h1',    text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }
+  end
+  
   describe "signup page" do
-    before { visit signup_path }
+    before { visit signup_path }    
+    let(:heading) {'Sign up'}
+    let(:page_title) {'Sign up'}
+
+    it_should_behave_like "all user actions"
     
-    it { should have_selector('h1', text: 'Sign up') }
-    it { should have_selector('title', text: full_title('Sign up')) }
   end
 end
