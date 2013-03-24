10e2f7e8c95fa168fd10d7e691a45b2cdfb16b77
rambz <rambz1@gmail.com>
2013-03-24 23:38:22 +0200

    halfway through sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  include SessionsHelper
+  
+  # Force signout to prevent CRSF attacks
+  def handle_unverified_request
+    sign_out
+  end
 end
